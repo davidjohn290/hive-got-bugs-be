66dbbf0f3d69ab1dcec3a579d82a3454a1bd5d79
David John <davidspc@MacBook-Air.local>
2020-09-14 15:24:54 +0100

    server structor and apiRouter created

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const apiRouter = express.Router();
+
+apiRouter.use("/users");
+
+apiRouter.use("/problems");
+
+apiRouter.use("/suggestions");
+
+apiRouter.use("/tech");
+
+module.exports = apiRouter;
