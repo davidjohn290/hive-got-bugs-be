8197780a32e78b85457c645b360689aebb31d6a7
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-09-14 21:27:06 +0100

    Link problems to user and tech data

--- index.js
+++ index.js
@@ -3,101 +3,100 @@ const shuffle = require("shuffle-array");
 const { sample } = require("lodash");
 
 const techList = [
-    "JavaScript",
-    "Python",
-    "Ruby",
-    "React",
-    "Java",
-    "TypeScript",
-    "Django",
-    "C++",
-    "C",
-    "C#.net",
-    "Redux",
-    "Vue.js",
-    "Express",
-    "Jest",
-    "SQL",
+  "JavaScript",
+  "Python",
+  "Ruby",
+  "React",
+  "Java",
+  "TypeScript",
+  "Django",
+  "C++",
+  "C",
+  "C#.net",
+  "Redux",
+  "Vue.js",
+  "Express",
+  "Jest",
+  "SQL",
 ];
 
+const getDateOneYearAgo = () => {
+  const yearAgo = new Date();
+  yearAgo.setMonth(yearAgo.getMonth() - 12);
+  console.log(yearAgo);
+  return yearAgo;
+};
+
 const generateTechData = (techList) => {
-    return techList.map((pieceOfTech) => {
-        return { slug: pieceOfTech };
-    });
+  return techList.map((pieceOfTech) => {
+    return { slug: pieceOfTech };
+  });
 };
 
 const generateUserData = (n) => {
-    const users = [];
-    const roleRef = { 0: "user", 1: "mentor" };
+  const users = [];
+  const roleRef = { 0: "user", 1: "mentor" };
 
-    for (let i = 0; i < n; i++) {
-        const shuffledSkills = shuffle(skills, { copy: true });
-        const role = roleRef[Math.round(Math.random())];
+  for (let i = 0; i < n; i++) {
+    const shuffledSkills = shuffle(techList, { copy: true });
+    const role = roleRef[Math.round(Math.random())];
 
-        const users = {
-            name: faker.name.findName(),
-            username: faker.internet.userName(),
-            avatarUrl: faker.image.avatar(),
-            onlineStatus: false,
-            role: role,
-            bugPoints: Math.round(Math.random() * 100),
-            bugPointsPrevMonth: Math.round(Math.random() * 10),
-            githubUrl: role === "mentor" ? `https://github.com/${faker.random.word()}` : null,
-            description: role === "mentor" ? faker.lorem.paragraph() : null,
-            skill1: role === "mentor" ? shuffledSkills[0] : null,
-            skill2: role === "mentor" ? shuffledSkills[1] : null,
-            skill3: role === "mentor" ? shuffledSkills[2] : null,
-            skill4: role === "mentor" ? shuffledSkills[3] : null,
-        };
-        users.push(userData);
-    }
+    const user = {
+      name: faker.name.findName(),
+      username: faker.internet.userName(),
+      avatarUrl: faker.image.avatar(),
+      onlineStatus: "false",
+      role: role,
+      bugPoints: Math.round(Math.random() * 100),
+      bugPointsPrevMonth: Math.round(Math.random() * 10),
+      githubUrl:
+        role === "mentor" ? `https://github.com/${faker.random.word()}` : null,
+      description: role === "mentor" ? faker.lorem.paragraph() : null,
+      skill1: role === "mentor" ? shuffledSkills[0] : null,
+      skill2: role === "mentor" ? shuffledSkills[1] : null,
+      skill3: role === "mentor" ? shuffledSkills[2] : null,
+      skill4: role === "mentor" ? shuffledSkills[3] : null,
+    };
+    users.push(user);
+  }
 
-    return users;
+  return users;
 };
 
-// const generateProblemData = (n, userdata) => {};
+const generateProblemData = (n, users, tech) => {
+  let problems = [];
+  const oneYearAgo = getDateOneYearAgo();
 
-const generateProblemData = (n) => {
-    // problem_id, title, user, body, difficulty: int, solved:bool, tech
-    let problems = [];
-    for (let i = 0; i < n; i++) {
-        const problem = {
-            title: faker.lorem.word(),
-            // user: ???
-            body: faker.lorem.sentence(),
-            difficulty: Math.round(Math.random() * 3),
-            solved: faker.random.boolean().toString(),
-            tech: faker.lorem.words(), //placeholder
-            date: faker.date.recent(),
-        };
-        problems.push(problem);
-    }
-
-    return problems;
+  for (let i = 0; i < n; i++) {
+    const problem = {
+      title: faker.lorem.word(),
+      user: sample(users).username,
+      body: faker.lorem.paragraph(),
+      difficulty: Math.round(Math.random() * 2),
+      solved: faker.random.boolean().toString(),
+      tech: sample(tech).slug,
+      date: faker.date.between(oneYearAgo, new Date()),
+    };
+    problems.push(problem);
+  }
+  return problems;
 };
 
-const generateSuggestionData = (n) => {
-    const suggestions = [];
-    for (let i = 0; i < n; i++) {
-        let suggestion = {
-            body: faker.lorem.sentence(),
-            approvedByMentor: false,
-        };
-        suggestions.push(suggestion);
-    }
-    return suggestions;
+const generateSuggestionData = (n, problems) => {
+  const suggestions = [];
+  for (let i = 0; i < n; i++) {
+    let suggestion = {
+      body: faker.lorem.sentence(),
+      approved: false,
+    };
+    suggestions.push(suggestion);
+  }
+  return suggestions;
 };
 
-const suggestions = generateSuggestionData(10);
+const tech = generateTechData(techList);
+const users = generateUserData(10);
+const problems = generateProblemData(20, users, tech);
+const suggestions = generateSuggestionsData(100, problems);
 
 console.log(suggestions);
-
-// const problems = generateProblemData(10);
-
-// console.log(problems);
-
-//const tech = generateTechData(techList);
-// const users = generateUserData(10);
-// const problems = generateProblemData(10, users)
-
-// Users, problems, suggestions, tech
