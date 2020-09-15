8ec2227148bcdac9a6f8ef26569673fd042d1929
David John <davidspc@MacBook-Air.local>
2020-09-15 09:21:13 +0100

    added start script

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const apiRouter = express.Router();
+const usersRouter = require("./usersRouter");
+const problemsRouter = require("./problemsRouter");
+const suggestionsRouter = require("./suggestionsRouter");
+const techRouter = require("./techRouter");
 
-apiRouter.use("/users");
+apiRouter.use("/users", usersRouter);
 
-apiRouter.use("/problems");
+apiRouter.use("/problems", problemsRouter);
 
-apiRouter.use("/suggestions");
+apiRouter.use("/suggestions", suggestionsRouter);
 
-apiRouter.use("/tech");
+apiRouter.use("/tech", techRouter);
 
 module.exports = apiRouter;
