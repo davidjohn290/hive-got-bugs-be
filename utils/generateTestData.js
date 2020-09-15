74ea9127c7d46aea1b97e5b5b449fd833f39c484
Kojjob <kodeon6@gmail.com>
2020-09-15 17:07:02 +0100

    Add generateTestData.js

--- utils/generateTestData.js
+++ utils/generateTestData.js
@@ -13,27 +13,27 @@ const mkdir = promisify(fs.mkdir);
 const writeFile = promisify(fs.writeFile);
 
 const tech = generateTechData(techList);
-const users = generateUserData(10);
-const problems = generateProblemData(20, users, tech);
-const suggestions = generateSuggestionData(10, problems, users);
+const users = generateUserData(5);
+const problems = generateProblemData(10, users, tech);
+const suggestions = generateSuggestionData(4, problems, users);
 
-mkdir("./db/data/dev-data")
+mkdir("./db/data/test-data")
     .then(() => {
-        return writeFile("./db/data/dev-data/tech.js", formatData(tech), "utf8");
+        return writeFile("./db/data/test-data/tech.js", formatData(tech), "utf8");
     })
     .then(() => {
-        return writeFile("./db/data/dev-data/users.js", formatData(users), "utf8");
+        return writeFile("./db/data/test-data/users.js", formatData(users), "utf8");
     })
     .then(() => {
         return writeFile(
-            "./db/data/dev-data/problems.js",
+            "./db/data/test-data/problems.js",
             formatData(problems),
             "utf8"
         );
     })
     .then(() => {
         return writeFile(
-            "./db/data/dev-data/suggestions.js",
+            "./db/data/test-data/suggestions.js",
             formatData(suggestions),
             "utf8"
         );
