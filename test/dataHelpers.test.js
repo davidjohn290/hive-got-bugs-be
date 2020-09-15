8fde0077a81e2dad943d48c57be0623a086a2b7e
Kojjob <kodeon6@gmail.com>
2020-09-15 15:56:31 +0100

    Working on user data - testing

--- test/dataHelpers.test.js
+++ test/dataHelpers.test.js
@@ -1,20 +1,39 @@
 const {
-  techList,
-  generateTechData,
-  generateUserData,
-  generateProblemData,
-  generateSuggestionData,
+    techList,
+    generateTechData,
+    generateUserData,
+    generateProblemData,
+    generateSuggestionData,
 } = require("../utils/dataHelpers");
 
 describe("generateDevData.js", () => {
-  test("formats tech data", () => {
-    const tech = generateTechData(techList);
-    tech.forEach((pieceOfTech) => {
-      expect(pieceOfTech).toEqual(
-        expect.objectContaining({
-          slug: expect.any(String),
-        })
-      );
+    test("formats tech data", () => {
+        const tech = generateTechData(techList);
+        tech.forEach((pieceOfTech) => {
+            expect(pieceOfTech).toEqual(
+                expect.objectContaining({
+                    slug: expect.any(String),
+                })
+            );
+        });
     });
-  });
+    test("User data", () => {
+        const user = generateUserData(5);
+        user.forEach((u) => {
+            expect(u).toEqual(
+                expect.objectContaining({
+                    name: expect.any(String),
+                    username: expect.any(String),
+                    avatar_url: expect.any(String),
+                    online_status: expect.any(String),
+                    role: expect.any(String),
+                    bug_points: expect.any(Number),
+                    bug_points_over_month: expect.any(Number),
+                    //TODO - description and skills 1 - 4
+                    //github_url: expect.any(),
+                    //description: expect(expect.any(String) || null),
+                })
+            );
+        });
+    });
 });
