7eb48a9a9010f9ce5a1fc6949791b5eb47d6fa2b
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-09-15 21:28:10 +0100

    Begin writing seed function

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,8 +1,20 @@
+const {
+  problemsData,
+  suggestionsData,
+  techData,
+  usersData,
+} = require("../data/index");
+
 exports.seed = function (knex) {
   return knex.migrate
     .rollback()
     .then(() => {
       return knex.migrate.latest();
     })
-    .then(() => {});
+    .then(() => {
+      const techInsertions = knex("tech").insert(techData);
+      const usersInsertions = knex("users").insert(usersData);
+      const problemsInsertions = knex("problems").insert(problemsData);
+      return Promise.all([techInsertions, usersInsertions, problemsInsertions]);
+    });
 };
