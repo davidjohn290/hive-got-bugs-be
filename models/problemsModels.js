7a9369bc6c3d74fd4e5534f22a76625366c23ded
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-09-16 19:30:41 +0100

    Pass first test

--- models/problemsModels.js
+++ models/problemsModels.js
@@ -1,5 +1,19 @@
 const knex = require("../db/connection");
+const { formatBooleans } = require("../utils/modelsHelpers");
 
-exports.selectProblems = () => {
-  return knex.select("*").from("problems");
+exports.selectProblems = (
+  sort_by = "created_at",
+  order = "desc",
+  solved = "false"
+) => {
+  return knex
+    .select("*")
+    .from("problems")
+    .orderBy(sort_by, order)
+    .modify((query) => {
+      if (solved) query.where("solved", "=", solved);
+    })
+    .then((problems) => {
+      return formatBooleans(problems);
+    });
 };
--- tests/problemsEndpoint.test.js
+++ tests/problemsEndpoint.test.js
@@ -1,5 +1,5 @@
-const app = require("../app");
-const request = require("../supertest");
+const app = require("../server");
+const request = require("supertest");
 const knex = require("../db/connection");
 
 describe("api/problems", () => {
@@ -10,16 +10,16 @@ describe("api/problems", () => {
       return request(app)
         .get("/api/problems")
         .expect(200)
-        .then(({ body: problems }) => {
-          expect(problems.length).toBe(10);
+        .then(({ body: { problems } }) => {
+          expect(problems.length).toBe(8);
           expect(problems).toBeSortedBy("created_at", { descending: true });
           problems.forEach((problem) => {
             expect(problem).toEqual(
               expect.objectContaining({
                 problem_id: expect.any(Number),
-                created_at: expect.any(Date),
+                created_at: expect.any(String),
                 difficulty: expect.any(Number),
-                solved: expect.toBe(false),
+                solved: false,
                 tech: expect.any(String),
                 title: expect.any(String),
                 body: expect.any(String),
