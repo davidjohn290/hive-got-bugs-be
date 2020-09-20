382f8120694d752d8c886283b5776d5cc126ea14
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-09-20 15:32:28 +0100

    Suppress FS-related options warning in console

--- knexfile.js
+++ knexfile.js
@@ -34,9 +34,9 @@ const customConfig = {
 };
 
 // Suppress "FS-related option specified..." warning in console
-// const log = console.log;
-// console.log = (...args) => {
-//   if (!/FsMigrations/.test(args[0])) log(...args);
-// };
+const log = console.log;
+console.log = (...args) => {
+  if (!/FsMigrations/.test(args[0])) log(...args);
+};
 
 module.exports = { ...customConfig[ENV], ...baseConfig };
