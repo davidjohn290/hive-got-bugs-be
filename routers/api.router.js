6f8cfa8acf37a848c14991ac367f8cf0878d6a8e
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-07 16:34:22 +0100

    Implement serving JSON description of API

--- routers/api.router.js
+++ routers/api.router.js
@@ -4,6 +4,10 @@ const usersRouter = require("./users.router");
 const problemsRouter = require("./problems.router");
 const suggestionsRouter = require("./suggestions.router");
 const techRouter = require("./tech.router");
+const { handle405s } = require("../errors");
+const { getAPIDescription } = require("../controllers/api.controllers");
+
+apiRouter.route("/").get(getAPIDescription).all(handle405s);
 
 apiRouter.use("/users", usersRouter);
 
